ae88d2eb79946b80872a1b21e2d11a4460e42c0c
Dongmin,Yoo <drew.yoo@gbike.io>
2023-03-18 03:14:21 +0900

    build(android): ⬆️ updated all gradle versions
    Mandatory permission request Created in
    AndroidManifest.xml, completed setup in
    build.gradle file.

--- src/NativeSegwayBleManager.ts
+++ src/NativeSegwayBleManager.ts
@@ -2,15 +2,25 @@ import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
-  init(secretKey: string, operatorCode: string, isDebug: boolean): void;
-  connect(bleMac: string, bleKey: string, iotImei: string): void;
-  disconnect(): void;
-  unlock(): void;
-  lock(): void;
-  vehicleInfo(): void;
-  openBatteryCover(): void;
-  openSaddle(): void;
-  openTailBox(): void;
+  init(
+    secretKey: string,
+    operatorCode: string,
+    isDebug: boolean,
+    callback: (msg: string) => void
+  ): void;
+  connect(
+    bleMac: string,
+    bleKey: string,
+    iotImei: string,
+    callback: () => void
+  ): void;
+  disconnect(callback: () => void): void;
+  unLock(callback: () => void): void;
+  lock(callback: () => void): void;
+  vehicleInfo(callback: () => void): void;
+  openBatteryCover(callback: () => void): void;
+  openSaddle(callback: () => void): void;
+  openTailBox(callback: () => void): void;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('SegwayBleManager');
