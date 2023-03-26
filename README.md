58a56a2f29146ab337925785fc5b3a1d6e3f3a6c
Dongmin,Yoo <drew.yoo@gbike.io>
2023-03-27 03:00:18 +0900

    feat(application): ⚡️ modified Example Application Codes
    The components of the example app were drastically modified to separate the components as much as possible, and code related to permission was also added as a separate file.

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # @gbike/segway-ble-manager
 
 `react-native-segway-ble-manager` is a React Native library for managing Bluetooth Low Energy (BLE) connections with Segway devices.
-
-`react-native-segway-ble-manager` provides an easy-to-use API for connecting to and communicating with Segway devices over BLE. The library supports both Android and iOS platforms and includes features such as device discovery, connection management, and data transfer. With `react-native-segway-ble-manager`, developers can easily integrate Segway device control into their React Native applications.
+`react-native-segway-ble-manager` provides an easy-to-use API for connecting to and communicating with Segway devices over BLE.
+The library supports both Android and iOS platforms and includes features such as device discovery, connection management, and data transfer.
+With `react-native-segway-ble-manager`, developers can easily integrate Segway device control into their React Native applications.
 
 ## Installation
 
@@ -12,12 +13,15 @@ npm install @gbike/segway-ble-manager
 
 ## Usage
 
-```js
-import { multiply } from '@gbike/segway-ble-manager';
-
-// ...
-
-const result = await multiply(3, 7);
+```xml
+    <!-- iOS 13 and newer, include the `NSBluetoothAlwaysUsageDescription` -->
+    <key>NSBluetoothAlwaysUsageDescription</key>
+    <string>스쿠터 조작을 위해 블루투스를 사용합니다</string>
+    <!-- iOS 12 and earlier, include `NSBluetoothPeripheralUsageDescription` -->
+    <key>NSBluetoothPeripheralUsageDescription</key>
+    <string>스쿠터 조작을 위해 블루투스를 사용합니다</string>
+    <key>NSLocationWhenInUseUsageDescription</key>
+    <string>스쿠터 조작을 위해 위치데이터를 접근합니다</string>
 ```
 
 ## Contributing
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,38 +1,65 @@
 import { Button, SafeAreaView, ScrollView, StatusBar, StyleSheet, View } from 'react-native';
 import React from 'react';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import Section from './Section';
-import { IoTInformation, Scooter } from '@gbike/segway-ble-manager';
+import InfoSection from './Section';
+import type { IoTInformation, Scooter, VehicleInfo } from '@gbike/segway-ble-manager';
 import {
-  // connect,
+  connect,
   disconnect,
+  init,
   unLock,
   lock,
   openBatteryCover,
   openSaddle,
   openTailBox,
   queryVehicleInformation,
-  queryIotInformation,
+  queryIoTInformation,
 } from '@gbike/segway-ble-manager';
+import ScooterButton from './Scooter';
+import scooters from './data/devices.json';
+import { getRequiredPermissions } from './permission';
+
+const BLE_OPERATION_CODE: string = process.env.BLE_OPERATION_CODE as string;
+const BLE_SECRET_KEY: string = process.env.BLE_SECRET_KEY as string;
 
 const App = () => {
-  const [iotInformation] = React.useState<IoTInformation>();
-  // const [vehicleInformation, setVehicleInformation] = React.useState<VehicleInfo>();
+  const [iotInformation, setIoTInformation] = React.useState<IoTInformation>();
+  const [vehicleInformation, setVehicleInformation] = React.useState<VehicleInfo>();
   const [scooter, setScooter] = React.useState<Scooter>({
     number: '356463',
-    bleMac: 'FA:B7:08:B5:B5:46',
-    bleKey: 'gBiKeWkd',
+    deviceMac: 'FA:B7:08:B5:B5:46',
+    deviceKey: 'gBiKeWkd',
     iotImei: '861123056350117',
   });
 
+  // lifecycle
+  React.useEffect(() => {
+    // instance 생성
+    init(BLE_SECRET_KEY, BLE_OPERATION_CODE, true);
+    // get required permissions
+    getRequiredPermissions();
+  }, []);
+
+  const getVehicleInformation = () => {
+    queryVehicleInformation((data) => {
+      setVehicleInformation(data);
+      console.log(JSON.stringify(vehicleInformation));
+    });
+  };
+
+  const getIoTInformation = () => {
+    queryIoTInformation((data) => {
+      setIoTInformation(data);
+    });
+  };
+
+  // ui renderer
   return (
     <SafeAreaView style={styles.backgroundStyle}>
       <StatusBar barStyle={'dark-content'} />
       <View>
         <View style={styles.buttonGroup}>
-          <Button onPress={disconnect} title="Disconnect" />
-          <Button onPress={queryVehicleInformation} title="Vehicle Info" />
-          <Button onPress={queryIotInformation} title="IoT Info" />
+          <Button onPress={() => connect(scooter.deviceMac, scooter.deviceKey, scooter.iotImei)} title="Connect" />
+          <Button onPress={() => disconnect()} title="Disconnect" />
         </View>
         <View style={styles.buttonGroup}>
           <Button onPress={unLock} title="UnLock" />
@@ -43,80 +70,29 @@ const App = () => {
           <Button onPress={openSaddle} title="Open Saddle" />
           <Button onPress={openTailBox} title="Open Tail Box" />
         </View>
+        <View style={styles.buttonGroup}>
+          <Button onPress={getVehicleInformation} title="Vehicle Info" />
+          <Button onPress={getIoTInformation} title="IoT Info" />
+        </View>
       </View>
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         contentContainerStyle={{ flex: 1 }}
         style={styles.backgroundStyle}
       >
-        <View>
-          <Section title="powerPercent">{iotInformation?.powerPercent}</Section>
-          <Section title="speedMode">{iotInformation?.speedMode}</Section>
-          <Section title="currentSpeed">{iotInformation?.currentSpeed}</Section>
-          <Section title="totalRange">{iotInformation?.totalRange}</Section>
-          <Section title="remainingRange">{iotInformation?.remainingRange}</Section>
-        </View>
-        <View style={{ height: 180, justifyContent: 'space-around' }}>
-          <Button
-            title="356463"
-            color={scooter.number === '356463' ? Colors.green : Colors.black}
-            onPress={() => {
-              setScooter({
-                number: '356463',
-                bleMac: 'FA:B7:08:B5:B5:46',
-                bleKey: 'gBiKeWkd',
-                iotImei: '861123056350117',
-              });
-            }}
-          />
-          <Button
-            title="361556"
-            color={scooter.number === '361556' ? Colors.green : Colors.black}
-            onPress={() => {
-              setScooter({
-                number: '361556',
-                bleMac: '78:05:41:1C:37:86',
-                bleKey: 'gBiKeWkd',
-                iotImei: '866770052403186',
-              });
-            }}
-          />
-          <Button
-            title="332884"
-            color={scooter.number === '332884' ? Colors.green : Colors.black}
-            onPress={() => {
-              setScooter({
-                number: '332884',
-                bleMac: '78:05:41:07:B3:51',
-                bleKey: 'gBiKeWkd',
-                iotImei: '860517048911822',
-              });
-            }}
-          />
-          <Button
-            title="356465"
-            color={scooter.number === '356465' ? Colors.green : Colors.black}
-            onPress={() => {
-              setScooter({
-                number: '356465',
-                bleMac: '78:05:41:0A:1F:48',
-                bleKey: 'gBiKeWkd',
-                iotImei: '860517046421444',
-              });
-            }}
-          />
-          <Button
-            title="356460"
-            color={scooter.number === '356460' ? Colors.green : Colors.black}
-            onPress={() => {
-              setScooter({
-                number: '356460',
-                bleMac: 'E5:41:48:9E:41:CC',
-                bleKey: 'gBiKeWkd',
-                iotImei: '867584035499958',
-              });
-            }}
-          />
+        <InfoSection iotInformation={iotInformation} />
+        <View style={styles.scooterGroup}>
+          {scooters.map((gco, _) => {
+            return (
+              <ScooterButton
+                selected={gco.number === scooter.number}
+                setScooter={setScooter}
+                scooter={gco}
+                style={undefined}
+                key={gco.number}
+              />
+            );
+          })}
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -133,7 +109,7 @@ const styles = StyleSheet.create({
     fontWeight: '600',
   },
   backgroundStyle: {
-    backgroundColor: Colors.lighter,
+    backgroundColor: '#F3F3F3',
     flex: 1,
   },
   buttonGroup: {
@@ -143,6 +119,10 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  scooterGroup: {
+    height: 180,
+    justifyContent: 'space-around',
   },
 });
 
