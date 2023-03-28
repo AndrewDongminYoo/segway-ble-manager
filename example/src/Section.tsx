c579d6676c2aa98fb94e1aae3a8d5d0f105053e0
Dongmin,Yoo <drew.yoo@gbike.io>
2023-03-28 14:57:57 +0900

    fix(application): 🚑️ accidently swapped type definitions - fixed
    I made a rudimentary mistake of writing code by changing the namespace of IoT information type and Vehicle information type. I made a hasty correction.

--- example/src/Section.tsx
+++ example/src/Section.tsx
@@ -6,7 +6,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
 import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { IoTInformation } from '@gbike/segway-ble-manager';
+import { IoTInformation, VehicleInfo } from '@gbike/segway-ble-manager';
 
 // ui renderer
 function Section({ children, title }: { children?: React.ReactNode; title: string }) {
@@ -18,14 +18,30 @@ function Section({ children, title }: { children?: React.ReactNode; title: strin
   );
 }
 
-function InfoSection({ iotInformation }: { iotInformation?: IoTInformation }) {
+function InfoSection({
+  iotInformation,
+  vehicleInformation,
+}: {
+  iotInformation?: IoTInformation;
+  vehicleInformation?: VehicleInfo;
+}) {
   return (
     <View>
-      <Section title="powerPercent">{iotInformation?.powerPercent}</Section>
-      <Section title="speedMode">{iotInformation?.speedMode}</Section>
-      <Section title="currentSpeed">{iotInformation?.currentSpeed}</Section>
-      <Section title="totalRange">{iotInformation?.totalRange}</Section>
-      <Section title="remainingRange">{iotInformation?.remainingRange}</Section>
+      <View>
+        <Section title="highBatteryVoltage">{iotInformation?.highBatteryVoltage}</Section>
+        <Section title="isLocked">{iotInformation?.isLocked}</Section>
+        <Section title="majorVersionNumber">{iotInformation?.majorVersionNumber}</Section>
+        <Section title="minorVersionNumber">{iotInformation?.minorVersionNumber}</Section>
+        <Section title="updateTimes">{iotInformation?.updateTimes}</Section>
+        <Section title="voltage">{iotInformation?.voltage}</Section>
+      </View>
+      <View>
+        <Section title="powerPercent">{vehicleInformation?.powerPercent}</Section>
+        <Section title="speedMode">{vehicleInformation?.speedMode}</Section>
+        <Section title="currentSpeed">{vehicleInformation?.currentSpeed}</Section>
+        <Section title="totalRange">{vehicleInformation?.totalRange}</Section>
+        <Section title="remainingRange">{vehicleInformation?.remainingRange}</Section>
+      </View>
     </View>
   );
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,39 @@
-const VALIDATION_TYPES: Record<string, RegExp> = {
-  'mac-address': /^([0-9A-F]{2}:){5}[0-9A-F]{2}$/gim,
-  'IMEI': /^[0-9]{15}$/gm,
-  'device-ble-key': /^[0-9A-Za-z]{8}$/gim,
-  'operator-code': /^[0-9A-Za-z]{6}$/gim, // ApiKey
-  'secret-key': /^[0-9a-z]{31}$/gim, // Bluetooth SDK Key
-} as const;
+const VALIDATION_TYPES = {
+  'mac-address': /^([A-Z0-9]{2}:){5}[A-Z0-9]{2}$/,
+  'IMEI': /\d{15}/,
+  'device-ble-key': /^\w{8}$/,
+  'operator-code': /^[A-Z0-9]{6}$/, // ApiKey
+  'secret-key': /^[0-9a-z]{32}$/, // Bluetooth SDK Key
+};
+
+const assert = (condition: boolean, message: string) => {
+  !condition && console.error(new Error(message));
+};
 
 type ValidationType = keyof typeof VALIDATION_TYPES;
+export function validateRegex(target: string, type: ValidationType) {
+  const re = VALIDATION_TYPES[type];
+  const isValid = RegExp(re, 'gm').test(target);
+  console.debug(`${type} ${target} is matching with ${re}gm?`, isValid);
+  return isValid;
+}
 
-export function validateRegex(input: string, type: ValidationType) {
-  const re = VALIDATION_TYPES[type] as RegExp;
-  console.debug('validateRegex', { input, type, re });
-  return re.test(input);
+export const validateScooter = (MAC_ADDRESS: string, DEV_BLE_KEY: string, IOT_IMEI: string) => {
+  console.log('checking key code...', { MAC_ADDRESS, DEV_BLE_KEY, IOT_IMEI });
+  const a = validateRegex(MAC_ADDRESS, 'mac-address');
+  const b = validateRegex(DEV_BLE_KEY, 'device-ble-key');
+  const c = validateRegex(IOT_IMEI, 'IMEI');
+  assert(a, 'Mac Address of device is not valid');
+  assert(b, 'Device BLE Key is not valid');
+  assert(c, "IoT's IMEI Code is not valid");
+  return a && b && c;
+};
+export function validateKeyCode(operatorCode: string, secretKey: string, isDebug: boolean) {
+  console.log('checking key code...', { operatorCode, secretKey, isDebug });
+  const a = validateRegex(operatorCode, 'operator-code');
+  const b = validateRegex(secretKey, 'secret-key');
+  console.debug('DEBUG MODE', isDebug);
+  assert(a, 'Operator Code is not valid');
+  assert(b, 'Secret Key is not valid');
+  return a && b;
 }
